a1389a24be82f27a2db2a1cd5e4b45c2bf103a1a
KemoKemo <t2wonderland@gmail.com>
2016-08-07 19:35:17 +0900

    プロジェクトテンプレートから作った初期状態。

---
+++ android/src/com/t2wonderland/kurona/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.t2wonderland.kurona.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.t2wonderland.kurona.KuronanDash;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KuronanDash(), config);
+	}
+}
---
+++ desktop/src/com/t2wonderland/kurona/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.t2wonderland.kurona.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.t2wonderland.kurona.KuronanDash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KuronanDash(), config);
+	}
+}
